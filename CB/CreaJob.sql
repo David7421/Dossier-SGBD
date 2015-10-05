e7659f042f40281220889edc86557a0db0761465
oceaneseel <oceane.seel@hotmail.com>
2015-10-05 17:12:28 +0200

    Job ok

--- CB/CreaJob.sql
+++ CB/CreaJob.sql
@@ -3,9 +3,9 @@ BEGIN
 	(
 		JOB_NAME => 'JOB_BACKUP',
 		JOB_TYPE => 'STORED_PROCEDURE',
-		JOB_ACTION => PACKAGECB.BACKUP,
+		JOB_ACTION => BACKUP,
 		START_DATE => SYSTIMESTAMP,
-		REPEAT_INTERVAL => 'FREQ=DAILY;BYHOUR=24',
+		REPEAT_INTERVAL => 'FREQ=DAILY;BYHOUR=0;BYMINUTE=0;BYSECOND=0;',
 		ENABLED => TRUE,
 		COMMENTS => 'Backup des nouveaux utilisateurs et leurs évaluations du jour de leur création.'
 	);
