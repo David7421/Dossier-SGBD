3ba66b118399749ad9c887984d9d924d8b834990
Jérôme Fink <jeromefink@hotmail.com>
2015-10-18 23:41:25 +0200

    progress

--- CB/RapportAmeliore.sql
+++ CB/RapportAmeliore.sql
@@ -1,5 +1,12 @@
 --RAPPORT FILM
 
+CREATE OR REPLACE LOCAL TEMPORARY TABLE genre_tmp
+(
+  id varchar2(1000),
+  nom varchar2(1000)
+)
+ON COMMIT DELETE ROWS;
+
 DECLARE
   fichierId  utl_file.file_type;
 
@@ -31,11 +38,19 @@ DECLARE
   cpt NUMBER;
   i NUMBER;
   valVide NUMBER;
+  parc NUMBER;
 
-  TYPE resultChain IS TABLE OF varchar2(200) INDEX BY BINARY_INTEGER;
+  TYPE resultChain IS TABLE OF varchar2(500) INDEX BY BINARY_INTEGER;
   valeursUniques resultChain;
+  chaineRegex resultChain;
+  id resultChain;
+  nom resultChain;
 
   requeteBlock varchar2(500);
+
+  genre varchar2(500);
+
+  resultParse OWA_TEXT.VC_ARR;
   
 BEGIN
   fichierId := utl_file.fopen ('MOVIEDIRECTORY', 'Rapport.txt', 'W');
@@ -92,19 +107,96 @@ BEGIN
     utl_file.put_line (fichierId, '   1000-QUANTILE:  ' || (donnee.quantile1000));
 
     IF(nomColonne(cpt).nom = 'STATUS' OR nomColonne(cpt).nom = 'CERTIFICATION') THEN
+
       requeteBlock := 'SELECT DISTINCT '|| nomColonne(cpt).nom || ' FROM MOVIES_EXT';
-      EXECUTE IMMEDIATE(requeteBlock) INTO valeursUniques;
+      EXECUTE IMMEDIATE(requeteBlock) BULK COLLECT INTO valeursUniques;
 
       utl_file.put_line(fichierId,'NOMBRE VALEURS UNIQUES : ' || valeursUniques.COUNT);
       utl_file.put_line(fichierId,'               VALEURS : ');
 
       FOR i IN valeursUniques.FIRST..valeursUniques.LAST LOOP
         utl_file.put_line(fichierId,'                      ' || valeursUniques(i));
-        valeursUniques.DELETE;
-      END LOOP;     
+      END LOOP;
+
+      valeursUniques.DELETE;
     END IF;
 
   END LOOP;
+
+  i := 1;
+  SELECT regexp_substr(genres, '^\[\[(.*)\]\]$', 1, 1, '', 1) BULK COLLECT INTO chaineRegex FROM movies_ext;
+
+  FOR cpt IN chaineRegex.FIRST..chaineRegex.LAST LOOP
+
+    IF(LENGTH(chaineRegex(cpt)) > 0) THEN
+      LOOP
+        genre := regexp_substr(chaineRegex(cpt), '(.*?)(\|\||$)', 1, i, '', 1);
+
+        EXIT WHEN genre IS NULL;
+
+        IF OWA_PATTERN.MATCH(genre, '^(.*),,(.*)$', resultParse) THEN
+          id(i) := resultParse(1);
+          nom(i) := resultParse(2);
+        END IF;
+
+        i := i+1;
+      END LOOP;
+
+      FOR parc IN id.FIRST..id.LAST LOOP
+        INSERT INTO genre_tmp VALUES(id(parc), nom(parc));
+      END LOOP;
+
+      i := 1;
+      id.DELETE;
+      nom.DELETE;
+
+    END IF;
+
+  END LOOP;
+
+  nomColonne.DELETE;
+
+  SELECT COLUMN_NAME, DATA_TYPE BULK COLLECT INTO nomColonne 
+  FROM user_tab_columns 
+  WHERE table_name='MOVIES_EXT';
+  
+  utl_file.put_line (fichierId, '');
+  utl_file.put_line (fichierId, '');
+  utl_file.put_line (fichierId, 'TABLE GENRE : ');
+
+  FOR cpt IN nomColonne.FIRST..nomColonne.LAST LOOP
+
+    utl_file.put_line (fichierId,'');
+    utl_file.put_line (fichierId, nomColonne(cpt).nom || ' :');
+
+    requeteBlock := 'SELECT MAX(LENGTH('||nomColonne(cpt).nom ||')), MIN(LENGTH('||nomColonne(cpt).nom ||')), 
+    AVG(LENGTH('||nomColonne(cpt).nom ||')), STDDEV(LENGTH('||nomColonne(cpt).nom ||')), 
+    MEDIAN(LENGTH('||nomColonne(cpt).nom ||')), COUNT('||nomColonne(cpt).nom ||'), 
+    PERCENTILE_CONT(0.99) WITHIN GROUP(ORDER BY LENGTH('||nomColonne(cpt).nom ||')), 
+    PERCENTILE_CONT(0.999) WITHIN GROUP(ORDER BY LENGTH('||nomColonne(cpt).nom ||')), 
+    COUNT(NVL2('||nomColonne(cpt).nom||', NULL, 1))
+    FROM MOVIES_EXT';
+
+    EXECUTE IMMEDIATE (requeteBlock) INTO donnee;
+
+    requeteBlock := 'SELECT COUNT(*) FROM MOVIES_EXT WHERE ' || nomColonne(cpt).nom || ' = '''' ';
+
+    EXECUTE IMMEDIATE (requeteBlock) INTO valVide;
+
+    utl_file.put_line (fichierId, '             MAX:  ' || donnee.max);
+    utl_file.put_line (fichierId, '             MIN:  ' || donnee.min);
+    utl_file.put_line (fichierId, '         MOYENNE:  ' || ROUND(donnee.avg,2));
+    utl_file.put_line (fichierId, '      ECART-TYPE:  ' || ROUND(donnee.ecart,2));
+    utl_file.put_line (fichierId, '         MEDIANE:  ' || ROUND(donnee.mediane,2));
+    utl_file.put_line (fichierId, '     NBR VALEURS:  ' || (donnee.totVal + donnee.valNull));
+    utl_file.put_line (fichierId, '    VALEURS NULL:  ' || donnee.valNull);
+    utl_file.put_line (fichierId, 'VALEURS NON NULL:  ' || donnee.totVal);
+    utl_file.put_line (fichierId, '   VALEURS VIDES:  ' || (valVide));
+    utl_file.put_line (fichierId, '    100-QUANTILE:  ' || (donnee.quantile100));
+    utl_file.put_line (fichierId, '   1000-QUANTILE:  ' || (donnee.quantile1000));
+
+  END LOOP;
+
 
   utl_file.fclose (fichierId);
 
