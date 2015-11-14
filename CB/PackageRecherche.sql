9ca0c6957865cfe2482d6484a08b5ee4440e8bc4
Jérôme Fink <jeromefink@hotmail.com>
2015-11-14 14:36:48 +0100

    debut recherche film

---
+++ CB/PackageRecherche.sql
@@ -0,0 +1,29 @@
+
+
+-------------------
+-- SPECIFICATION --
+-------------------
+
+CREATE OR REPLACE PACKAGE PACKAGERECHERCHE
+IS
+
+END;
+/
+
+
+
+
+----------
+-- BODY --
+----------
+
+CREATE OR REPLACE PACKAGE BODY PACKAGERECHERCHE
+IS
+
+
+
+
+END;
+/
+
+EXIT;
