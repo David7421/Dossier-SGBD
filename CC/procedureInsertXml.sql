507942b4dba8974ba9bc5f1481a4f50844a11b15
Jérôme Fink <jeromefink@hotmail.com>
2015-12-13 09:43:51 +0100

    plus de pertes de copies

--- CC/procedureInsertXml.sql
+++ CC/procedureInsertXml.sql
@@ -4,10 +4,18 @@ BEGIN
 
 	--On va d'abord renvoyer les copies qui ne sont plus utilisées.
 
+	LOGEVENT('RECEPTION_FILM', 'debut de la reception');
+
 	INSERT INTO tmpXMLCopy SELECT * FROM copieFilm;
 
+	--On supprime les copies retournée de CC
+	DELETE FROM COPIEFILM 
+	WHERE EXTRACTVALUE(object_value , 'copie/idFilm') IN (SELECT EXTRACTVALUE(XML_COL , 'copie/idFilm') FROM tmpXMLCopy)
+	AND EXTRACTVALUE(object_value , 'copie/numCopy') IN (SELECT EXTRACTVALUE(XML_COL , 'copie/numCopy') FROM tmpXMLCopy);
+
+	--Insertion des copies mis en attente sur CB
 	INSERT INTO FILMSCHEMA SELECT * FROM tmpXMLMovie@CB.DBL
-  	WHERE EXTRACTVALUE(XML_COL , 'film/if_film') NOT IN (select extractvalue(object_value, 'film/id_film') FROM FILMSCHEMA);
+  	WHERE EXTRACTVALUE(XML_COL , 'film/id_film') NOT IN (select extractvalue(object_value, 'film/id_film') FROM FILMSCHEMA);
 
 	INSERT INTO COPIEFILM SELECT * FROM tmpXMLCopy@CB.DBL;
 
@@ -17,8 +25,8 @@ BEGIN
 	COMMIT;
 
 	RETOUR_COPIE@CB.DBL;
-	
 
+	LOGEVENT('RECEPTION_FILM', 'Fin de la reception');
 EXCEPTION
 	WHEN OTHERS THEN LOGEVENT('procedure reception', 'ERREUR : ' ||SQLERRM); ROLLBACK;
 END;
