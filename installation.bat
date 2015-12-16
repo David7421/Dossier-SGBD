355937b73eb49c65ecc76c2bd44f6bee8cd7e35a
oceaneseel <oceane.seel@hotmail.com>
2015-12-16 08:45:00 +0100

    pause et echo + db link admin config

--- installation.bat
+++ installation.bat
@@ -1,19 +1,43 @@
+echo SYS
+
 sqlplus / as sysdba @Sys/scriptCreationSys.sql
+
+echo CB1
 
 sqlplus CB/CB@localhost:1521/xe @CB/ScriptCreationCB1.sql
 
+echo CBB1
+
 sqlplus CBB/CBB@localhost:1521/xe @CBB/ScriptCreationCBB1.sql
+
+echo CB2
 
 sqlplus CB/CB@localhost:1521/xe @CB/ScriptCreationCB2.sql
 
+echo CBB2
+
 sqlplus CBB/CBB@localhost:1521/xe @CBB/ScriptCreationCBB2.sql
+
+echo CC1
 
 sqlplus CC/CC@localhost:1521/xe @CC/ScriptCreationCC1.sql
 
+echo CB3
+
 sqlplus CB/CB@localhost:1521/xe @CB/ScriptCreationCB3.sql
+
+echo CBB3
 
 sqlplus CBB/CBB@localhost:1521/xe @CBB/ScriptCreationCBB3.sql
 
+echo CC2
+
 sqlplus CC/CC@localhost:1521/xe @CC/ScriptCreationCC2.sql
 
+echo CB4
+
 sqlplus CB/CB@localhost:1521/xe @CB/ScriptCreationCB4.sql
+
+echo INSTALLATION TERMINEE
+
+pause
