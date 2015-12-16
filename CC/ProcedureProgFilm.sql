517b1116946b295266ca5058b33bc808b44dee44
Jérôme Fink <jeromefink@hotmail.com>
2015-12-16 10:21:20 +0100

    recolte des prog films

--- CC/ProcedureProgFilm.sql
+++ CC/ProcedureProgFilm.sql
@@ -1,15 +1,36 @@
 create or replace PROCEDURE PROGFILM
 AS	
-	programmation XMLTYPE;	
+ 	TYPE tabCol IS TABLE OF xmltype INDEX BY BINARY_INTEGER;
+	tabProgra tabCol;	
+  	
+	cpt number;
+	isValid number;
 
+  	tmp varchar2(20);
 BEGIN
 
---with xmlt AS()
---	SELECT * FROM xmltable('programmations/progra' passing BFILE('MOVIEDIRECTORY', nls_charset_id('AL32UTF8')))
---	);
+    WITH xmlt(value) AS(
+      SELECT * FROM xmltable('programmations/progra' 
+      passing xmltype(BFILENAME('MOVIEDIRECTORY', 'programmations/progra.xml'), nls_charset_id('AL32UTF8')))
+    )
+    SELECT * bulk collect into tabProgra FROM xmlt;
+    
+  
+  	cpt := tabProgra.FIRST;
+	WHILE cpt IS NOT NULL
+	LOOP
+		--isValid := tabProgra(cpt).isSchemaValid('http://cc/prograEntrante.xsd');
 
-	select XMLTYPE(BFILENAME('MOVIEDIRECTORY', 'programmations/progra.xml'), nls_charset_id('AL32UTF8')) INTO programmation
-	FROM DUAL;
+		SELECT XMLISVALID(tabProgra(cpt), 'http://cc/prograEntrante.xsd') INTO isValid
+		FROM DUAL;
+
+		DBMS_OUTPUT.PUT_LINE('test : '||isValid);
+
+		
+
+		
+    	cpt := tabProgra.NEXT(cpt);
+	END LOOP;
 
 EXCEPTION
 	WHEN OTHERS THEN LOGEVENT('procedure reception', 'ERREUR : ' ||SQLERRM); ROLLBACK;
