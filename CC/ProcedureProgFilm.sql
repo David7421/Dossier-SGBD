ff4ffeee93dc372955494db9f3c0fbcdf4e6bce2
Jérôme Fink <jeromefink@hotmail.com>
2015-12-16 19:16:05 +0100

    recup des données dans le XML envoye

--- CC/ProcedureProgFilm.sql
+++ CC/ProcedureProgFilm.sql
@@ -6,7 +6,11 @@ AS
 	cpt number;
 	isValid number;
 
-  	XmlValueBuffer varchar2(20);
+  	idFilm varchar2(20);
+  	idCopie varchar2(20);
+  	idSalle varchar2(20);
+  	heure varchar2(20);
+
   	resultTest varchar2(2);
 
 BEGIN
@@ -25,7 +29,7 @@ BEGIN
 		FROM DUAL;
 
 		DBMS_OUTPUT.PUT_LINE('test : '||isValid);
-
+		--Si le tag idFilm existe
 		IF(tabProgra(cpt).extract('progra/idFilm/text()') IS NULL) THEN
 			DBMS_OUTPUT.PUT_LINE('NULL');
 			--Pas d'ID film : progra non valide
@@ -34,19 +38,62 @@ BEGIN
 			CONTINUE;
 		END IF;
 
-		XmlValuebuffer := tabProgra(cpt).extract('progra/idFilm/text()').getStringVal();
+		idFilm := tabProgra(cpt).extract('progra/idFilm/text()').getStringVal();
 
-		--Test de la valeur de retour
+		--test de la copie du film
+		IF(tabProgra(cpt).extract('progra/numCopy/text()') IS NULL) THEN
+			DBMS_OUTPUT.PUT_LINE('numcopynull');
+			--Pas d'ID film : progra non valide
+			--TO DO FEEDBACK
+			cpt := tabProgra.NEXT(cpt);
+			CONTINUE;
+		END IF;
+
+		idCopie := tabProgra(cpt).extract('progra/numCopy/text()').getStringVal();
+
+		--récupération du numero de salle
+
+		IF(tabProgra(cpt).extract('progra/numSalle/text()') IS NULL) THEN
+			DBMS_OUTPUT.PUT_LINE('numcopynull');
+			--Pas d'ID film : progra non valide
+			--TO DO FEEDBACK
+			cpt := tabProgra.NEXT(cpt);
+			CONTINUE;
+		END IF;
+
+		idSalle := tabProgra(cpt).extract('progra/numSalle/text()').getStringVal();
+
+
+		--récupération de la date et de l'heure du debut de sceance
+
+		IF(tabProgra(cpt).extract('progra/heureDebut/text()') IS NULL) THEN
+			DBMS_OUTPUT.PUT_LINE('numcopynull');
+			--Pas d'ID film : progra non valide
+			--TO DO FEEDBACK
+			cpt := tabProgra.NEXT(cpt);
+			CONTINUE;
+		END IF;
+
+		heure := tabProgra(cpt).extract('progra/heureDebut/text()').getStringVal();
+
+
+		--La copie existe-t-elle sur CC?
 		BEGIN
-			SELECT 'ok' INTO resultTest FROM FILMSCHEMA
-			WHERE XmlValuebuffer = extractvalue(object_value, 'film/id_film');
+			SELECT 'ok' INTO resultTest FROM COPIEFILM
+			WHERE idFilm = extractvalue(object_value, 'copie/idFilm')
+			AND idCopie = extractvalue(object_value, 'copie/numCopy');
 		EXCEPTION
 			WHEN NO_DATA_FOUND THEN resultTest := 'ko';
 		END;
 
+		IF(resultTest = 'ko') THEN
+			DBMS_OUTPUT.PUT_LINE('copie de film pas sur CC invalide');
+			--Pas d'ID film : progra non valide
+			--TO DO FEEDBACK
+			cpt := tabProgra.NEXT(cpt);
+			CONTINUE;
+		END IF;
 		
-
-		DBMS_OUTPUT.PUT_LINE(resultTest);
 
     	cpt := tabProgra.NEXT(cpt);
 	END LOOP;
