b4c1777442784cbdfea35409cac5cd55a35d5c52
Jérôme Fink <jeromefink@hotmail.com>
2015-12-16 22:23:18 +0100

    enregistrement xml + feedback

--- CC/ProcedureProgFilm.sql
+++ CC/ProcedureProgFilm.sql
@@ -9,9 +9,12 @@ AS
   	idFilm varchar2(20);
   	idCopie varchar2(20);
   	idSalle varchar2(20);
-  	heure varchar2(20);
+  	heureDebut varchar2(20);
 
   	resultTest varchar2(2);
+
+  	nomFichierFeedback varchar2(50) := 'programmations/feedback.xml';
+  	xmlFeedBack xmltype := xmltype('<programmations></programmations>');
 
 BEGIN
 
@@ -20,8 +23,9 @@ BEGIN
       passing xmltype(BFILENAME('MOVIEDIRECTORY', 'programmations/progra.xml'), nls_charset_id('AL32UTF8')))
     )
     SELECT * bulk collect into tabProgra FROM xmlt;
+
+    LOGEVENT('PROGFILM', 'Debut');
     
-  
   	cpt := tabProgra.FIRST;
 	WHILE cpt IS NOT NULL
 	LOOP
@@ -31,9 +35,14 @@ BEGIN
 		DBMS_OUTPUT.PUT_LINE('test : '||isValid);
 		--Si le tag idFilm existe
 		IF(tabProgra(cpt).extract('progra/idFilm/text()') IS NULL) THEN
-			DBMS_OUTPUT.PUT_LINE('NULL');
+
+			LOGEVENT('PROGFILM','XML sans idFilm');
 			--Pas d'ID film : progra non valide
-			--TO DO FEEDBACK
+			select INSERTCHILDXML(tabProgra(cpt), 'progra', 'feedback', xmltype('<feedback>Vous devez indiquer une balise idFilm</feedback>'))
+			INTO tabProgra(cpt) FROM DUAL;
+
+			--On construit le xml feedback
+			select INSERTCHILDXML(xmlFeedBack, 'programmations', 'progra', tabProgra(cpt)) INTO xmlFeedBack FROM DUAL;
 			cpt := tabProgra.NEXT(cpt);
 			CONTINUE;
 		END IF;
@@ -42,9 +51,14 @@ BEGIN
 
 		--test de la copie du film
 		IF(tabProgra(cpt).extract('progra/numCopy/text()') IS NULL) THEN
-			DBMS_OUTPUT.PUT_LINE('numcopynull');
-			--Pas d'ID film : progra non valide
-			--TO DO FEEDBACK
+
+			LOGEVENT('PROGFILM','XML sans numCopy');
+			--Pas de num copy: progra non valide
+			select INSERTCHILDXML(tabProgra(cpt), 'progra', 'feedback', xmltype('<feedback>Vous devez indiquer une balise numCopy</feedback>'))
+			INTO tabProgra(cpt) FROM DUAL;
+			
+			--Construction du XML feedback
+			select INSERTCHILDXML(xmlFeedBack, 'programmations', 'progra', tabProgra(cpt)) INTO xmlFeedBack FROM DUAL;
 			cpt := tabProgra.NEXT(cpt);
 			CONTINUE;
 		END IF;
@@ -54,9 +68,14 @@ BEGIN
 		--récupération du numero de salle
 
 		IF(tabProgra(cpt).extract('progra/numSalle/text()') IS NULL) THEN
-			DBMS_OUTPUT.PUT_LINE('numcopynull');
-			--Pas d'ID film : progra non valide
-			--TO DO FEEDBACK
+
+			LOGEVENT('PROGFILM','XML sans numSalle');
+			--Pas de num salle: progra non valide
+			select INSERTCHILDXML(tabProgra(cpt), 'progra', 'feedback', xmltype('<feedback>Vous devez indiquer une balise numSalle</feedback>'))
+			INTO tabProgra(cpt) FROM DUAL;
+
+			--Construction du XML feedback
+			select INSERTCHILDXML(xmlFeedBack, 'programmations', 'progra', tabProgra(cpt)) INTO xmlFeedBack FROM DUAL;
 			cpt := tabProgra.NEXT(cpt);
 			CONTINUE;
 		END IF;
@@ -67,15 +86,19 @@ BEGIN
 		--récupération de la date et de l'heure du debut de sceance
 
 		IF(tabProgra(cpt).extract('progra/heureDebut/text()') IS NULL) THEN
-			DBMS_OUTPUT.PUT_LINE('numcopynull');
-			--Pas d'ID film : progra non valide
-			--TO DO FEEDBACK
+			
+			LOGEVENT('PROGFILM','XML sans heureDebut');
+			--Pas de num salle: progra non valide
+			select INSERTCHILDXML(tabProgra(cpt), 'progra', 'feedback', xmltype('<feedback>Vous devez indiquer une balise heureDebut</feedback>'))
+			INTO tabProgra(cpt) FROM DUAL;
+
+			--construction XML 
+			select INSERTCHILDXML(xmlFeedBack, 'programmations', 'progra', tabProgra(cpt)) INTO xmlFeedBack FROM DUAL;
 			cpt := tabProgra.NEXT(cpt);
 			CONTINUE;
 		END IF;
 
-		heure := tabProgra(cpt).extract('progra/heureDebut/text()').getStringVal();
-
+		heureDebut := tabProgra(cpt).extract('progra/heureDebut/text()').getStringVal();
 
 		--La copie existe-t-elle sur CC?
 		BEGIN
@@ -87,16 +110,26 @@ BEGIN
 		END;
 
 		IF(resultTest = 'ko') THEN
-			DBMS_OUTPUT.PUT_LINE('copie de film pas sur CC invalide');
-			--Pas d'ID film : progra non valide
-			--TO DO FEEDBACK
+
+			LOGEVENT('PROGFILM','La copie choisie n existe pas dans CC');
+			--La copie n'existe pas
+			select INSERTCHILDXML(tabProgra(cpt), 'progra', 'feedback', xmltype('<feedback>La copie choisie pour la diffusion n''est pas en stock</feedback>'))
+			INTO tabProgra(cpt) FROM DUAL;
+
+			select INSERTCHILDXML(xmlFeedBack, 'programmations', 'progra', tabProgra(cpt)) INTO xmlFeedBack FROM DUAL;
 			cpt := tabProgra.NEXT(cpt);
 			CONTINUE;
 		END IF;
-		
 
+		--Heure de fin de la projection
+		select INSERTCHILDXML(xmlFeedBack, 'programmations', 'progra', tabProgra(cpt)) INTO xmlFeedBack FROM DUAL;
     	cpt := tabProgra.NEXT(cpt);
 	END LOOP;
+
+
+	--Ecriture dans le fichier feedback
+	DBMS_XSLPROCESSOR.CLOB2FILE(xmlFeedBack.getClobVal(), 'MOVIEDIRECTORY', nomFichierFeedback, nls_charset_id('AL32UTF8'));
+
 
 EXCEPTION
 	WHEN OTHERS THEN LOGEVENT('procedure reception', 'ERREUR : ' ||SQLERRM); ROLLBACK;
